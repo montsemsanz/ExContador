16b157afa3990658e94676623f100918d656640f
Montse Viyuela <montsemsanz@gmail.com>
2021-10-30 16:55:40 +0200

    Versión1 de Contador

---
+++ Contador.java
@@ -0,0 +1,51 @@
+
+/**
+ * Modela un contador
+ * 
+ * @author 
+ * @version 
+ */
+public class Contador {
+    private int valor;
+
+    /**
+     * Constructorde la clase Contador
+     */
+    public Contador()   {  
+       valor = 0;
+    }
+    
+    /**
+     * accesor
+     */
+    public int getContador()    {
+        return valor;
+    }
+    
+    /**
+     * mutador, incrementa en uno el contador
+     */
+    public void incrementar()   {
+         
+    }
+    
+    /**
+     * mutador, decrementa en uno el contador
+     */
+    public void decrementar()   {
+             
+    }
+    
+  
+     /**
+     * mutador, inicializa (hace un reset)
+     * a 0 el contador
+     */
+    public void reset()    {
+            valor = 0;
+    }
+    
+    
+    
+    
+}
