21be324385b3a5d430ee6d842a65d0000882ece3
mprog1920 <54936260+mprog1920@users.noreply.github.com>
2021-11-11 17:08:58 +0100

    Modificado bucle for

--- Contador.java
+++ Contador.java
@@ -46,7 +46,7 @@ public class Contador {
 
     public static void main(String[] args) {
         Contador c = new Contador();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             c.incrementar();
 
         }
